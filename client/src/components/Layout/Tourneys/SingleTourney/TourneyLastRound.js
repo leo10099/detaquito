89981941a364ed14f81625c74412d738b2fd2fe2
leo10099 <hancleandro@gmail.com>
2018-10-07 21:53:15 -0300

    styled tourney for mobile and fetching its name from api

--- client/src/components/Layout/Tourneys/SingleTourney/TourneyLastRound.js
+++ client/src/components/Layout/Tourneys/SingleTourney/TourneyLastRound.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import { connect } from "react-redux";
 
-export default class TourneyTotalPoints extends Component {
+class TourneyLastRound extends Component {
+  state = {};
+
+  componentDidMount() {
+    axios
+      .get(
+        `/api/fetch/tourney/ranked/lastround/?_id=${
+          this.props.match.params._id
+        }&current=${this.props.conf.round}`
+      )
+      .then(response => {
+        this.setState({
+          tourney: response.data.tourney,
+          scores: response.data.scores
+        });
+      });
+  }
   render() {
-    return <div className="text-white">TOURNEY LAST ROUND POINTS</div>;
+    return (
+      <section className="text-white">
+        TOURNEY LAST {this.props.conf.round} POINTS
+      </section>
+    );
   }
 }
+
+const mapStatetoProps = ({ conf }) => {
+  return { conf };
+};
+
+export default connect(
+  mapStatetoProps,
+  null
+)(TourneyLastRound);
--- client/src/components/Layout/Tourneys/SingleTourney/index.js
+++ client/src/components/Layout/Tourneys/SingleTourney/index.js
@@ -7,7 +7,8 @@ import {
 } from "react-router-dom";
 import { connect } from "react-redux";
 import { colors } from "../../../Utilities";
-
+import { PulseLoader } from "react-spinners";
+import axios from "axios";
 import TotalPoints from "./TourneyTotalPoints";
 import LastRoundPoints from "./TourneyLastRound";
 
@@ -17,15 +18,30 @@ class SingleTourney extends Component {
   state = {
     showLastRound: true
   };
+
+  componentDidMount() {
+    axios
+      .get(`/api/fetch/tourney/${this.props.match.params.tourney}/`)
+      .then(response => {
+        this.setState({ tourneyName: response.data.tourney.name });
+      });
+  }
+
   render() {
     const _id = this.props.match.params.tourney;
-    const { pathname } = this.props.location;
-
+    const { tourneyName } = this.state;
+    if (!tourneyName) {
+      return (
+        <div className="loading-container">
+          <PulseLoader color={colors.white} sizeUnit="rem" size={0.8} />;
+        </div>
+      );
+    }
     return (
       <Router>
         <section className="Single__Tourney">
           <h1 className="dashboard__title">Mis Torneos</h1>
-          <h3 className="dashboard__subtitle">Nombre del torneo</h3>
+          <h3 className="dashboard__subtitle">{tourneyName}</h3>
           <nav className="Single__Tourney__nav">
             <Link
               to={`/tournament/${_id}/totalpoints/`}
@@ -41,7 +57,7 @@ class SingleTourney extends Component {
               <i className="fa fa-list-ol baseline" />
               &nbsp; Tabla de Posiciones
             </Link>
-            <span className="text-white"> || </span>
+            <span className="text-white Single__Tourney__separator"> || </span>
             <Link
               to={`/tournament/${_id}/lastroundpoints/`}
               onClick={() => {
@@ -62,8 +78,8 @@ class SingleTourney extends Component {
             exact
           />
           <Route
-            path="/tournament/:_id/lastroundpoints/"
             component={LastRoundPoints}
+            path="/tournament/:_id/lastroundpoints/"
             exact
           />
         </section>
